95f88a9bb90ab16cbc430fc2ec9e505948449d9b
Björn Welboren <52087548+bwelboren@users.noreply.github.com>
2022-03-17 14:28:11 +0100

    Update grab.ps1

--- grab.ps1
+++ grab.ps1
@@ -1,3 +1,3 @@
 $str1 = (Invoke-WebRequest -Uri "https://www.fragpit.org/maps.php").Links.Href | Select-String -Pattern 'maps'
-mkdir C:/Users/bwelb/Desktop/maps # Ensure the folder you create is named 'maps'
+mkdir "C:/Users/bwelb/Desktop/maps" # Ensure the folder you create is named 'maps'
 foreach ($atr in $str1){Invoke-WebRequest -Uri "https://fragpit.org/$atr" -Outfile "C:/Users/bwelb/Desktop/$atr"}
